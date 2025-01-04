9d5494989adb1ab35f5724263f310194d74f7683
myson <myson>
2025-01-04 11:26:47 +0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="bitgravityzone",
-    version="0.0.2",
+    version="0.0.3",
     author="son",
     author_email="son@example.com",
     description="GravityZone API",
